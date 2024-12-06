72164b89551c99841e1dddcf93ab189ffffb9cbc
jazgill <jazzgill585@gmail.com>
2024-12-06 13:17:06 -0800

    implementing colorful memory match game functionality.  Added colorful_memory_game.html and colorfulGame.css and colorful_memory_game.js

--- colorfulMemoryGame/colorful_memory_game.js
+++ colorfulMemoryGame/colorful_memory_game.js
@@ -86,3 +86,19 @@ function startGameTimer(timeLeft) {
 
 startbtn.addEventListener('click', startGame);
 
+const personJSON = {
+    "name": "John Doe",
+    "age" : 30,
+    "email": "john@example.com",
+    "address": {
+        "street": "123 Main St",
+        "city": "Anytown",
+        "country": "USA"
+    },
+    "tags": ["JavaScript", "Node.js", "Web Development"],
+    "isStudent": false,
+    "WorkExperience": null
+};
+
+const jsonString = JSON.stringify(personJSON);
+console.log(jsonString);
