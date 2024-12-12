fce4af297719e94abb0c4a8266e183d6b50ae084
jazgill <jazzgill585@gmail.com>
2024-12-11 16:27:39 -0800

    Added functionality to check the weather by city name.  Utilized API calls to openweathermap website and also utilizing promise

--- weatherReport/weather_report.js
+++ weatherReport/weather_report.js
@@ -13,5 +13,10 @@ function showweatherDetails(event) {
                               <p>Temperature: ${data.main.temp} &#8451;</p>
                               <p>Weather: ${data.weather[0].description}</p>`;
     })
+    .catch(error => {
+        console.error('Error fetching weather: ', error);
+        const weatherInfo = document.getElementById('weatherInfo');
+        weatherInfo.innerHTML = `<p>Failded to fetch weather.  Please try again, Later.</p>`;
+    })
 }
 document.getElementById('weatherForm').addEventListener('submit',showweatherDetails);
